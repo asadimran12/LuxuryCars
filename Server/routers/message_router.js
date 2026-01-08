46d0c799ff927175e13fce8e5eecf3d7b9740d9b
Asad Imran <iasad4855@gmail.com>
2026-01-08 14:39:34 +0500

    fix error

--- Server/routers/message_router.js
+++ Server/routers/message_router.js
@@ -10,14 +10,16 @@ const driver_middleware = require("../middleware/driver_middleware");
 
 const router = express.Router();
 
+// Static routes first
 router.get("/chat/user", auth_middleware, GetChat);
-
 router.get("/chat/driver", driver_middleware, GetChat);
 
-router.get("/:senderId/:receiverId", auth_middleware, getMessages);
+// Routes with static prefix before dynamic params
 router.get("/driver/:senderId/:receiverId", driver_middleware, getMessages);
-
-router.delete("/:messageId", auth_middleware, deleteMessage);
 router.delete("/driver/:messageId", driver_middleware, deleteMessage);
+
+// Dynamic parameter routes last (catch-all)
+router.get("/:senderId/:receiverId", auth_middleware, getMessages);
+router.delete("/:messageId", auth_middleware, deleteMessage);
 
 module.exports = router;
